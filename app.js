74142fa735e905dc3063bc9c3ac3f881f913d74e
Muhammad Wali Zafri <135381982+Waliii31@users.noreply.github.com>
2024-02-26 02:08:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -474,6 +474,46 @@ function show_magicians(array) {
 }
 show_magicians(magician_names);
 // 42) Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+function make_great(magicians) {
+    var greatMagicians = [];
+    for (var _i = 0, magicians_1 = magicians; _i < magicians_1.length; _i++) {
+        var magician = magicians_1[_i];
+        greatMagicians.push("the Great " + magician);
+    }
+    return greatMagicians;
+}
 // 43) Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+var magicianNamesCopy = magician_names.slice();
+var greatMagicianNames = make_great(magicianNamesCopy);
+show_magicians(magician_names);
+show_magicians(greatMagicianNames);
 // 44) Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+function make_sandwich() {
+    var items = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        items[_i] = arguments[_i];
+    }
+    console.log("Sandwich Order:");
+    for (var _a = 0, items_1 = items; _a < items_1.length; _a++) {
+        var item = items_1[_a];
+        console.log("- " + item);
+    }
+}
+make_sandwich("Turkey", "Cheese", "Lettuce", "Tomato");
+make_sandwich("Ham", "Swiss Cheese", "Pickles");
+make_sandwich("Tuna", "Mayonnaise");
 // 45) Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+function carInfo(manufacturer, model) {
+    var options = [];
+    for (var _i = 2; _i < arguments.length; _i++) {
+        options[_i - 2] = arguments[_i];
+    }
+    var car = {
+        manufacturer: manufacturer,
+        model: model,
+        options: options
+    };
+    return car;
+}
+var myCar = carInfo("Toyota", "Camry", "Silver", "Sunroof", "Navigation System");
+console.log(myCar);
